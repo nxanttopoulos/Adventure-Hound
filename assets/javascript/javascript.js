0391fba966052a4333f2e19c3297a47e47b85720
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-04-02 19:11:18 -0400

    many changes

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,41 +9,55 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database(); 
 $(document).ready(function() { 
-    database.ref().on('value', function(snapshot) {
-    $(".trow").remove();
-    console.log(snapshot.val());
+  database.ref().on('value', function(snapshot) {
+  $(".trow").remove();
+  // console.log(snapshot.val());
     snapshot.forEach(function (child) { 
       var tableRow = $("<tr class='trow'></tr>");
-      // var key = child.key();
       var childData = child.val();
-      // console.log(key);
-      console.log(childData);
-      // $(".trow").empty();
+      // console.log(childData);
       $(".table").append(tableRow);
       tableRow.append("<td>"+childData.query+"</td>");
     });
   });
 });
-var query = "";
+
 $(".btn").on("click", function(event) {
   event.preventDefault();
   var query = $("#placeName").val().trim();
   console.log(query);
-  var tableRow = $("<tr class ='trow'></tr>");
-  $(".table").append(tableRow);
-  tableRow.append("<td>"+query+"</td>");
-  database.ref().push({
-    query:query,
-  });
+  // var tableRow = $("<tr class ='trow'></tr>");
+  // $(".table").append(tableRow);
+  // tableRow.append("<td>"+query+"</td>");
+  // database.ref().push({
+  //   query:query,
+  // });
 });
-
-// var queryURL = "http://www.omdbapi.com/?s=" + query + "&y=&plot=short&r=json";
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).done(function(response) {
-//   console.log(response);
-//   for (var i=0; i < response.Search.length; i++) {
-//     console.log(response.Search[i]);
-//   }
-// });
+// $(".btn").on("click", function(event) {
+//   event.preventDefault();
+//   var APIKey = "166a433c57516f51dfab1f7edaed8413";
+//   var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+//         "q="+ query +"&units=imperial&appid=" + APIKey;
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//   .done(function(response) {
+//     // Log the queryURL
+//     console.log(queryURL);
+//     // Log the resulting object
+//     console.log(response);
+//     // Transfer content to HTML
+//     <div class="city"></div>
+//     <div class="wind"></div>
+//     <div class="humidity"></div>
+//     <div class="temp"></div>
+//     $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+//     $(".wind").html("Wind Speed: " + response.wind.speed);
+//     $(".humidity").html("Humidity: " + response.main.humidity);
+//     $(".temp").html("Temperature (F) " + response.main.temp);
+//     // Log the data in the console as well
+//     console.log("Wind Speed: " + response.wind.speed);
+//     console.log("Humidity: " + response.main.humidity);
+//     console.log("Temperature (F): " + response.main.temp);
+//   });
