d5e92b8ff03850f8032c2997f40a3935495ba80d
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-04-07 20:14:22 -0400

    many updates

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -25,8 +25,15 @@ $(document).ready(function() {
       var data = dataArray[i];
       // console.log(dataArray);
       // console.log(data);
-      if (data === dataArray[i-1]) {
+      if (data === dataArray[i-2]) {
+        console.log(dataArray);
         dataArray.pop();
+        dataArray.pop();
+        console.log(dataArray);
+      } else if (data === dataArray[i-1]) {
+        console.log(dataArray);
+        dataArray.pop();
+        console.log(dataArray);
       } else {
         $(".table").append(tableRow);
         tableRow.append("<td id='queries'>"+dataArray[i]+"</td>");
@@ -37,9 +44,13 @@ $(document).ready(function() {
   $(".pushBtn").on("click", function(event) {
     event.preventDefault();
     var query = $("#placeName").val().trim();
-    database.ref().push({
-      query:query,
-    });
+    if (query === "") {
+      return;
+    } else {
+      database.ref().push({
+        query:query,
+      });
+    }
   });
   // Button - Weather API
   $("#weather").on("click", function(event) {
@@ -55,20 +66,50 @@ $(document).ready(function() {
     })
     .done(function(response) {
       var kelvin = response.main.temp;
-      var Fahrenheit = (kelvin-273.15) * 1.80 + 32;
+      var conditions = response.weather;
+      var forecast = conditions[0];
+      var Fahrenheit = parseInt((kelvin-273.15) * 1.80 + 32);
       var city = $("<div class='city'></div>");
       var wind = $("<div class='wind'></div>");
       var humidity = $("<div class='humidity'></div>");
       var temp = $("<div class='temp'></div>");
+      var cloud = $("<div class='cloud'></div>");
+      var rain = $("<div class='rain'></div>");
       $("#display_2").append(city);
+      $("#display_2").append(temp);
       $("#display_2").append(wind);
       $("#display_2").append(humidity);
-      $("#display_2").append(temp);
+      $("#display_2").append(rain);
       $(".city").html("<h2>" + response.name + " - Weather Details</h2>");
-      $(".wind").html("Wind Speed: " + response.wind.speed);
-      $(".humidity").html("Humidity: " + response.main.humidity);
-      $(".temp").html("Temperature (F) " + Fahrenheit);
+      $(".temp").html("Today's Forecast: " + forecast.description);
+      $(".rain").html("Temperature: "+ Fahrenheit + " (F)");
+      $(".wind").html("Wind Speed: " + response.wind.speed + " (MPH) ");
+      $(".humidity").html("Humidity: " + response.main.humidity+"%");
+      $(".cloud").html("Cloudiness: "+ response.clouds.all+"%");
     });
+    // var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" +
+    //       "q="+ query +"&appid=" + APIKey;
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET"
+    // })
+    // .done(function(response) {
+    //   // var kelvin = response.main.temp;
+    //   // var Fahrenheit = (kelvin-273.15) * 1.80 + 32;
+    //   var forecast = $("<div class='forecast'></div>");
+    //   var forecastM= $("<div class='mon'></div>");
+    //   var forecastT = $("<div class='tues'></div>");
+    //   var forecastW = $("<div class='wed'></div>");
+    //   var forecastH = $("<div class='thurs'></div>");
+      // $("#display_2").append(forecast);
+      // $("#display_2").append(forecastT);
+      // $("#display_2").append(forecastW);
+      // $("#display_2").append(forecastH);
+      // $(".forecast").html("<h3>" + response.city.name + " - Weekly Forecast</h3>");
+      // $(".temp").html("Today's Forecast: " + Fahrenheit);
+      // $(".wind").html("Wind Speed: " + response.wind.speed);
+      // $(".humidity").html("Humidity: " + response.main.humidity);
+    // });
   });
   // Button - Currency Exchange API 
   $("#currency").on("click", function(event) {
@@ -84,12 +125,20 @@ $(document).ready(function() {
       var ratesObj = response.rates;
       var exchangeRates = JSON.stringify(response.rates);
       var ratesDivHead = $("<div class='rateH'></div>");
-      var ratesDivBody1 = $("<table id='rateB1'></table>");
-      var ratesDivBody2 = $("<table id='rateB2'></table>");
+      var ratesDivBody1 = $("<table class ='displayRates' id='rateB1'></table>");
+      var ratesDivBody2 = $("<table class ='displayRates' id='rateB2'></table>");
+      var ratesDivBody3 = $("<table class ='displayRates' id='rateB3'></table>");
+      var ratesDivBody4 = $("<table class ='displayRates' id='rateB4'></table>");
+      var ratesDivBody5 = $("<table class ='displayRates' id='rateB5'></table>");
+      var ratesDivBody6 = $("<table class ='displayRates' id='rateB6'></table>");
       var tableRow = $("<tr></tr>");
       $("#display_2").append(ratesDivHead);
       $("#display_2").append(ratesDivBody1);
       $("#display_2").append(ratesDivBody2);
+      $("#display_2").append(ratesDivBody3);
+      $("#display_2").append(ratesDivBody4);
+      $("#display_2").append(ratesDivBody5);
+      $("#display_2").append(ratesDivBody6);
       $(".rateH").html("<h2>ExchangeRates"+response.base+"("+response.date+")</h2>");
       var ratesArray = [];
       for (var key in ratesObj) {
@@ -98,14 +147,35 @@ $(document).ready(function() {
       }
       // console.log(keysArray);
       // console.log(ratesArray);
-      for (i=0; i < keysArray.length; i++) {
+      for (i=0; i < 15; i++) {
+        console.log(keysArray.length);
         var tableRow = $("<tr></tr>");
         $("#rateB1").append(tableRow);
         tableRow.append("<td>"+keysArray[i]+"</td>");
       }
-      for (i=0; i < ratesArray.length; i++) {
+      for (i=0; i < 15; i++) {
         var tableRow = $("<tr></tr>");
         $("#rateB2").append(tableRow);
+        tableRow.append("<td> $"+ratesArray[i]+"</td>");
+      }
+      for (i=15; i < 30; i++) {
+        var tableRow = $("<tr></tr>");
+        $("#rateB3").append(tableRow);
+        tableRow.append("<td>"+keysArray[i]+"</td>");
+      }
+      for (i=15; i < 30; i++) {
+        var tableRow = $("<tr></tr>");
+        $("#rateB4").append(tableRow);
+        tableRow.append("<td> $"+ratesArray[i]+"</td>");
+      }
+      for (i=30; i < keysArray.length; i++) {
+        var tableRow = $("<tr></tr>");
+        $("#rateB5").append(tableRow);
+        tableRow.append("<td>"+keysArray[i]+"</td>");
+      }
+      for (i=30; i < ratesArray.length; i++) {
+        var tableRow = $("<tr></tr>");
+        $("#rateB6").append(tableRow);
         tableRow.append("<td> $"+ratesArray[i]+"</td>");
       }
     });
